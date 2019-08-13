ac3f67c468b81ed72c6c3846a85b980f8a44ad2d
FazioNico <contact@nicolasfazio.ch>
2019-08-13 12:22:46 +0200

    demo simple routing

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -4,14 +4,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
+import { InfoComponent } from './containers/info/info.component';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab2Page }])
+    RouterModule.forChild([
+      { path: '',
+        children: [
+        {
+          path: '',
+          component: Tab2Page
+        },
+        {
+          path: ':id',
+          component: InfoComponent
+        }
+      ]}
+    ])
   ],
-  declarations: [Tab2Page]
+  declarations: [
+    Tab2Page,
+    InfoComponent
+  ]
 })
 export class Tab2PageModule {}
